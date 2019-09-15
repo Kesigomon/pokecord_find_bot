ec546bfe4b2137dd8cc58251746e1c3a30a3b827
Kesigomon <34025522+Kesigomon@users.noreply.github.com>
2019-09-15 17:40:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Main(commands.Cog):
             return
         print("pokecord!")
         embed = message.embeds[0]
-        if embed.title == "A wild pok\xe9mon has appeared!":
+        if embed.title == "A wild pokémon has appeared!":
             key = await self.do_hash(embed.image.url)
             await message.channel.send(f"このポケモンの名前は{self.data[key]}です")
 
